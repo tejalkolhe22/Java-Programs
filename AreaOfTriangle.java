6baa9593486fa3aef4898b0e86c71e8f78f4b93a
Tejal Kolhe <kolhetejal22@gmail.com>
2024-08-08 20:34:40 +0530

    Add files via upload

---
+++ AreaOfTriangle.java
@@ -0,0 +1,16 @@
+package com.edu;
+
+public class AreaOfTriangle {
+     public static void main(String[] args) {
+	        int base, height;
+	        float area;
+	        base = 5;
+	        height = 10;
+	        
+	        area = 0.5f * base * height;
+	        System.out.println("The area of triangle of base = " + base + " and height = " + height + " is " + area);
+	    }
+	}
+
+	
+	
