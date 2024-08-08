df6f700437341d175f70e94646ae1af6a2dc617e
Tejal Kolhe <kolhetejal22@gmail.com>
2024-08-08 21:52:41 +0530

    Add files via upload

--- Array.java
+++ Array.java
@@ -1,27 +1,39 @@
+package com.edu;
+
 import java.util.Scanner;
 
 public class Array {
-
 	public static void main(String[] args) {
-	
-		        int ar[]=new int[4]; //declaration of an array
-		       // int []ar=new int[5];
-		        Scanner sc = new Scanner(System.in);
-		        
-		        System.out.println("Enter "+ar.length+"  array elements");
-		        for(int i=0;i<ar.length;i++) {
-		            ar[i] = sc.nextInt();
-		        }
-		        
-		        //Display array elements
-		        
-		        for(int i=0;i<ar.length;i++) {
-		            System.out.println(ar[i]);
-		        }
-		        
-		    }
-		
 
-	}
+        int sum=0;
+        int a[]=new int[8];
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter "+a.length+" elements");
+        for(int i=0;i<a.length;i++) {
+            a[i]=sc.nextInt();
+        }
+        
+      
+        System.out.println("Array elements are");
+        for(int i=0;i<a.length;i++) {
+            //System.out.println("a["+i+"]="+a[i]);
+            System.out.println(a[i]); 
+        }
+  
+        for(int i=0;i<a.length;i++)
+        {
+            sum=sum+a[i]; 
+        }
+        
+        System.out.println("Sum of all array elements "+sum);
+        
+        float avg =(float)sum/a.length;  
+        System.out.println("Average of all array elements "+avg);
+    }
+
+
+}
+
 
 
