dc9861db7c58de3bfdcddae8d7ea4b3095f0e807
Tejal Kolhe <kolhetejal22@gmail.com>
2024-08-08 20:46:59 +0530

    Add files via upload

---
+++ DJE.java
@@ -0,0 +1,21 @@
+package com.edu;
+
+public class DJE {
+
+		  public static void main(String[] args) {
+	          System.out.println();
+	          System.out.println("           ******           *************        **********");
+	          System.out.println("           **    **                **            **");
+	          System.out.println("           **     **               **            **");
+	          System.out.println("           **      **              **            **");
+	          System.out.println("           **      **              **            ********");
+	          System.out.println("           **      **       **     **            **");
+	          System.out.println("           **     **         **    **            **");
+	          System.out.println("           **    **           **  **             **");
+	          System.out.println("           *****               ****              **********");
+	          System.out.println();
+	       }  // end main()
+
+	
+
+}
