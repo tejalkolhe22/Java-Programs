a49a3ad5aa162fe5cfa0fd6e2518acb74945009b
Tejal Kolhe <kolhetejal22@gmail.com>
2024-08-08 20:38:25 +0530

    Add files via upload

---
+++ FirstApllicationdemo.java
@@ -0,0 +1,10 @@
+package com.edu;
+
+public class FirstApllicationdemo {
+
+	public static void main(String args[])
+	{
+		System.out.println("Hello" +2+3);
+		System.out.println(2+3+"hello");
+	}
+}
