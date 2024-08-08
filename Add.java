8c9d96711704c415da2878b6a2f365b29545690d
Tejal Kolhe <kolhetejal22@gmail.com>
2024-08-08 20:42:22 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,25 @@
+package com.edu;
+
+public class Add {
+
+	public static void main(String[] args) {
+		
+		int n;
+		float fval;
+		double dval;
+		char ch;
+		String name;
+		
+		n=10;
+		fval=5.6f;
+		dval=6.78;
+		ch='T';
+		name="Moni";
+        System.out.println("integer value n=" +n);
+        System.out.println("float value fval=" +fval);
+        System.out.println("double value d=" +dval);
+        System.out.println("char value ch=" +ch);
+        System.out.println("string value name=" +name);
+	}
+
+}
