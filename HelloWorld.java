4edf4648e9cc4aa81138517ace10ef219c890667
Tejal Kolhe <kolhetejal22@gmail.com>
2024-08-08 20:32:16 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.edu;
+
+public class HelloWorld {
+  public static void main(String args[]) {
+	  System.out.println("Hello Sonal");
+	  System.out.println("Bangalore");
+	  System.out.println("India");
+	  System.out.println("Jalgaon");
+  }
+}
