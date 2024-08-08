fef91120ea0ea801f814eb93fc0f6ee94aa86709
Tejal Kolhe <kolhetejal22@gmail.com>
2024-08-08 20:35:30 +0530

    Add files via upload

---
+++ AreaofRectangle.java
@@ -0,0 +1,14 @@
+package com.edu;
+
+public class AreaofRectangle {
+
+	public static void main(String[] args) {
+		int length = 8; // you can change this value to any length
+        int width = 5;  // you can change this value to any width
+        int area = length * width;
+        
+        System.out.println("The area of a rectangle with length = " + length + " and width = " + width + " is " + area);
+
+	}
+
+}
