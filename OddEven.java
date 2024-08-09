429cd4f7cb30dde7471699b59aa174057345c36b
Tejal Kolhe <kolhetejal22@gmail.com>
2024-08-09 11:47:21 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,18 @@
+package com.LargestOf2Numb;
+
+import java.util.Scanner;
+
+public class OddEven {
+	public static void main(String[] args) {
+		 Scanner reader = new Scanner(System.in);
+
+	        System.out.print("Enter a number: ");
+	        int num = reader.nextInt();
+
+	        if(num % 2 == 0)
+	            System.out.println(num + " is even");
+	        else
+	            System.out.println(num + " is odd");
+	}
+	
+}
