a7911eee4acad6c7eab68a2aba0072641c4c5c0e
Tejal Kolhe <kolhetejal22@gmail.com>
2024-08-09 11:43:21 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,24 @@
+package com.edu;
+
+public class odd {
+
+	public static void main(String[] args) {
+	  int num=0;
+	  if(num>0)
+		{
+			System.out.println("The number is positive");
+			
+		}
+		else if(num<0)
+		{
+			System.out.println("The number is negative");
+			
+		}
+
+		else
+		{
+			System.out.println("The given number is zero" );
+		}
+	}
+
+}
